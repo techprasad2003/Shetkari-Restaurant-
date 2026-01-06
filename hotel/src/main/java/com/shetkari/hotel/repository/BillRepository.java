f51ee6b65fab8a7a29f1f2aa8cd5ed5b55a81793
Prasad Gaikwad <147265309+techprasad2003@users.noreply.github.com>
2026-01-06 22:01:20 +0530

    Delete hotel directory

--- hotel/src/main/java/com/shetkari/hotel/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shelkari.hotel.repository;
-
-import com.shelkari.hotel.model.Bill;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface BillRepository extends MongoRepository<Bill, String> {
-    Optional<Bill> findByGuestId(String guestId);
-}
--- hotel/src/main/java/com/shetkari/hotel/repository/GuestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shelkari.hotel.repository;
-
-import com.shelkari.hotel.model.Guest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface GuestRepository extends MongoRepository<Guest, String> {
-    List<Guest> findByIdIn(List<String> ids);
-}
